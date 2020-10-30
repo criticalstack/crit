e502f3286b0b8d9eeee90cb89e4a047f7ed3e289
chris <chris@couch.life>
2020-10-30 12:59:19 -0400

    Improve and conslidate cinder node build inputs
    This moves all of the input values used to build the cinder image into the internal/cinder/config/constants package. This ensures that updating versions that go into the cinder image will only need to take place there.
    
    This also changes the way the Kubernetes version is set. Rather than being hardcoded in a const, it is determined when creating a new node by reading the /cinder/kubernetes_version file.
    
    The Kind base image has also been updated and a fix has been applied to account for changes in the image. The newer versions of the image include the kubelet systemd files which means that the /etc/default/kubelet file was already created.

--- internal/cinder/config/constants/constants.go
+++ internal/cinder/config/constants/constants.go
@@ -6,21 +6,29 @@ import (
 	"github.com/criticalstack/crit/pkg/config/constants"
 )
 
+var (
+	DefaultNodeImage = "criticalstack/cinder:latest"
+)
+
 const (
-	DefaultNodeImage  = "criticalstack/cinder:v1.0.6"
-	DefaultNetwork    = "cinder"
+	// the Kubernetes version to beused when building the cinder image
 	KubernetesVersion = "1.18.10"
 
-	DefaultMachineAPIVersion               = "1.0.6"
-	DefaultMachineAPIProviderDockerVersion = "1.0.7"
-	DefaultKubeRBACProxyVersion            = "0.5.0"
-	DefaultCiliumVersion                   = "1.8.5"
-	DefaultCiliumStartupScriptVersion      = "af2a99046eca96c0138551393b21a5c044c7fe79"
-	DefaultLocalPathProvisionerVersion     = "0.0.12"
-	DefaultRegistryVersion                 = "2.7.1"
+	// Kind base image to use as the base for the cinder image
+	KindBaseImage = "docker.io/kindest/base:v20200928-02f74589"
 
+	DefaultNetwork           = "cinder"
 	DefaultLocalRegistryName = "cinderegg"
 	DefaultLocalRegistryPort = 5000
+
+	// container image versions
+	CiliumStartupScriptVersion      = "af2a99046eca96c0138551393b21a5c044c7fe79"
+	CiliumVersion                   = "1.8.5"
+	KubeRBACProxyVersion            = "0.5.0"
+	LocalPathProvisionerVersion     = "0.0.12"
+	MachineAPIProviderDockerVersion = "1.0.7"
+	MachineAPIVersion               = "1.0.6"
+	RegistryVersion                 = "2.7.1"
 )
 
 func GetImages() map[string]string {
@@ -33,14 +41,14 @@ func GetImages() map[string]string {
 		"coredns":                     fmt.Sprintf("%s:%s", constants.CoreDNSImage, constants.DefaultCoreDNSVersion),
 		"bootstrap-server":            fmt.Sprintf("%s:v%s", constants.CritBootstrapServerImage, constants.DefaultBootstrapServerVersion),
 		"healthcheck-proxy":           fmt.Sprintf("%s:v%s", constants.CritHealthCheckProxyImage, constants.DefaultHealthcheckProxyVersion),
-		"machine-api":                 fmt.Sprintf("docker.io/criticalstack/machine-api:v%s", DefaultMachineAPIVersion),
-		"machine-api-provider-docker": fmt.Sprintf("docker.io/criticalstack/machine-api-provider-docker:v%s", DefaultMachineAPIProviderDockerVersion),
-		"kube-rbac-proxy":             fmt.Sprintf("gcr.io/kubebuilder/kube-rbac-proxy:v%s", DefaultKubeRBACProxyVersion),
-		"cilium":                      fmt.Sprintf("docker.io/cilium/cilium:v%s", DefaultCiliumVersion),
-		"cilium-operator-generic":     fmt.Sprintf("docker.io/cilium/operator-generic:v%s", DefaultCiliumVersion),
-		"cilium-startup-script":       fmt.Sprintf("docker.io/cilium/startup-script:%s", DefaultCiliumStartupScriptVersion),
-		"local-path-provisioner":      fmt.Sprintf("docker.io/rancher/local-path-provisioner:v%s", DefaultLocalPathProvisionerVersion),
-		"registry":                    fmt.Sprintf("docker.io/library/registry:%s", DefaultRegistryVersion),
+		"machine-api":                 fmt.Sprintf("docker.io/criticalstack/machine-api:v%s", MachineAPIVersion),
+		"machine-api-provider-docker": fmt.Sprintf("docker.io/criticalstack/machine-api-provider-docker:v%s", MachineAPIProviderDockerVersion),
+		"kube-rbac-proxy":             fmt.Sprintf("gcr.io/kubebuilder/kube-rbac-proxy:v%s", KubeRBACProxyVersion),
+		"cilium":                      fmt.Sprintf("docker.io/cilium/cilium:v%s", CiliumVersion),
+		"cilium-operator-generic":     fmt.Sprintf("docker.io/cilium/operator-generic:v%s", CiliumVersion),
+		"cilium-startup-script":       fmt.Sprintf("docker.io/cilium/startup-script:%s", CiliumStartupScriptVersion),
+		"local-path-provisioner":      fmt.Sprintf("docker.io/rancher/local-path-provisioner:v%s", LocalPathProvisionerVersion),
+		"registry":                    fmt.Sprintf("docker.io/library/registry:%s", RegistryVersion),
 	}
 	return images
 }
